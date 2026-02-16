75ac0a029cb35fe9692089fd670a9f6ec370e898
Amey-Kalsapnavar <ameykalsapnavar@email.com>
2026-02-16 16:35:34 +0530

    Connected FastAPI backend with MongoDB Atlas

---
+++ backend/app/database.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+MONGO_URL = "mongodb+srv://ameyk_db_user:<Ameykalsapnavar2004>@cluster0.yyqousx.mongodb.net/?appName=Cluster0"
+
+client = MongoClient(MONGO_URL)
+
+db = client["recommendation_db"]
--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Backend is running"}
+
+
+from app.database import db
+
+@app.get("/test-db")
+def test_db():
+    return {"message": "Database connected successfully"}
