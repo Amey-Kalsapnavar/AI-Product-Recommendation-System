cfb82485788a3bba04635d4f3378abea8a998d79
Amey-Kalsapnavar <ameykalsapnavar@email.com>
2026-02-16 16:44:36 +0530

    format update in main.py and database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-MONGO_URL = "mongodb+srv://ameyk_db_user:<Ameykalsapnavar2004>@cluster0.yyqousx.mongodb.net/?appName=Cluster0"
+MONGO_URL = "mongodb+srv://ameyk_db_user:Ameykalsapnavar2004@cluster0.yyqousx.mongodb.net/?appName=Cluster0"
 
 client = MongoClient(MONGO_URL)
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
+from app.database import db
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Backend is running"}
-
-
-from app.database import db
 
 @app.get("/test-db")
 def test_db():
